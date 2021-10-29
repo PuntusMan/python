9ceaec3b3104e2e9bfe583cb3ea6c4416f204b51
PuntusMan <88797298+PuntusMan@users.noreply.github.com>
2021-10-29 21:47:51 +0500

    Add files via upload

---
+++ 8.1.py
@@ -0,0 +1,13 @@
+import re
+
+def email_parse(email_addres):
+    re_email = re.compile(r'(?P<username>([\w]+))@(?P<domain>[^&]+\. \w+)', re.IGNORECASE)
+    if not re_email.match(email_addres):
+        raise ValueError(f'wrong email: {email_addres}')
+    print(re_email.match(email_addres).groupdict())
+
+for i in ['someone@geekbrains.ru', 'som&eone@geekbrainsru', 'someone@geekbrainsru']:
+    try:
+        email_parse(i)
+    except ValueError as err:
+        print(err)
